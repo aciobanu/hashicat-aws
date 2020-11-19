ec072012301e6540512d4d2245158cf41e737739
root <root@p-s73tkzhvjucx-workstation.c.instruqt-prod.internal>
2020-11-19 12:25:23 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,10 +3,6 @@
 module "s3_bucket" {
   source  = "app.terraform.io/aciobanu-training/s3-bucket/aws"
   version = "1.15.0"
-
-  acceleration_status = "Suspended"
   bucket = "my-s3-bucket"
   bucket_prefix = "${var.prefix}"
-  policy = {}
-  request_payer = "BucketOwner"
 }
