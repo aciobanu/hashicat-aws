54f1986051a9f30ecf6b247f7a6f47910d45d529
root <root@p-pvlxti4uiilk-workstation.c.instruqt-prod.internal>
2020-11-20 15:08:23 +0000

    Don't care about s3 anymore

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------------
 // Modules
-module "s3_bucket" {
-  source  = "app.terraform.io/aciobanu-training/s3-bucket/aws"
-  version = "1.15.0"
-  bucket_prefix = "${var.prefix}"
-}
+#module "s3_bucket" {
+#  source  = "app.terraform.io/aciobanu-training/s3-bucket/aws"
+#  version = "1.15.0"
+#  bucket_prefix = "${var.prefix}"
+#}
